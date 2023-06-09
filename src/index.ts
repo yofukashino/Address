0a9540104af21e873ef155defa960d7e9995759f
Tharki-god <alokender018@gmail.com>
2023-06-09 19:25:50 +0530

    v0.9.5 pnpm update

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ export const PluginLogger = Logger.plugin("Address");
 export const SettingValues = await settings.init("dev.tharki.Address", defaultSettings);
 import { AddressMenuItem } from "./Components/MenuItem";
 
-import { HBCM } from "./lib/HomeButtonContextMenuApi";
+import HBCM from "./lib/HomeButtonContextMenuApi";
 
 export const start = (): void => {
   registerSettings();
